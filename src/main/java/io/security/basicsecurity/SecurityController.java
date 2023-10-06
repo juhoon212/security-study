82d8dc491470acaa5caaee69e2b91a3305294c96
juhoon212 <ws5501@naver.com>
2023-10-06 20:43:18 +0900

    security login

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecurityController {
 
+    // 시큐리티에서 제공하는 로그인 화면
     @GetMapping("/")
     public String index() {
         return "home";
