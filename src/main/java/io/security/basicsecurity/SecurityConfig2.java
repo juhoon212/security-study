191a11b666d41bd43c9558ea517899bcbe293531
juhoon212 <ws5501@naver.com>
2023-11-26 23:51:39 +0900

    다중 클래스 사용시 @Order로 순서적용 가능

--- src/main/java/io/security/basicsecurity/SecurityConfig2.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig2.java
@@ -2,12 +2,14 @@ package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Order(1) //순서
 public class SecurityConfig2 {
 
     @Bean
@@ -19,13 +21,14 @@ public class SecurityConfig2 {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
-        
+
         return http.build();
     }
 
 }
 
 @Configuration
+@Order(0)
 class SecurityConfig3 {
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
